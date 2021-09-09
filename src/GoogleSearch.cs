519fab2b64dafc6c349e63a0e30b5326e858762d
Tim Hanewich <tahanewich@live.com>
2021-09-09 18:37:55 -0400

    Added core structure

---
+++ src/GoogleSearch.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Net;
+using System.Net.Http;
+
+namespace TimHanewich.Google
+{
+    public class GoogleSearch
+    {
+        public SearchResult[] Results {get; set;}
+    }
+}
---
+++ src/SearchResult.cs
@@ -0,0 +1,12 @@
+using System;
+using System.IO;
+
+namespace TimHanewich.Google
+{
+    public class SearchResult
+    {
+        public string Title {get; set;}
+        public string Description {get; set;}
+        public string Url {get; set;}
+    }
+}
